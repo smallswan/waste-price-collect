9e50a2b503bf1a723b990073266ff5e9a05db126
huangshunle <huangshunle_gxu@163.com>
2018-03-29 11:18:00 +0800

    删除不必要的接口

--- src/main/java/com/zhiluniao/jobs/dao/CollectTaskRespository.java
+++ src/main/java/com/zhiluniao/jobs/dao/CollectTaskRespository.java
@@ -2,14 +2,12 @@ package com.zhiluniao.jobs.dao;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.zhiluniao.jobs.model.entity.CollectTaskEntity;
 
-public interface CollectTaskRespository extends PagingAndSortingRepository<CollectTaskEntity, Integer>,JpaRepository<CollectTaskEntity, Integer>, JpaSpecificationExecutor<CollectTaskEntity>{
+public interface CollectTaskRespository extends JpaRepository<CollectTaskEntity, Integer>, JpaSpecificationExecutor<CollectTaskEntity>{
 
     public CollectTaskEntity findByRemoteUri(String remoteUri);
     
